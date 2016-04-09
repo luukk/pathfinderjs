0411c570e5798a9ab13cec0effb51ac3fb5f5bc3
luukk <klaverluuk@gmail.com>
2016-04-09 16:19:57 +0200

    sets the heuristic and the movement cost

--- a-star.js
+++ a-star.js
@@ -12,27 +12,36 @@ var astar = {
     teken.addEventListener('click',function(){
       var grid = self.getGrid();
       self.pushStartingSquare(grid);
+      self.heuristicDistance(grid);
+    //  self.AdjecantSquares(grid);
     });
   },
   pushStartingSquare:function(grid){
-    var startpoint = grid.startpoint[0];
-    this.parentsquares.push(startpoint);
-    this.getAdjecantSquares(grid.grid);
   },
-  getAdjecantSquares:function(grid){
-    //first array in the closedlist is the parent
-    var tileGrid = grid;
-    var parentNode = this.parentsquares[0];
-    x = parseInt(parentNode.id.substr(0,parentNode.id.indexOf(',')));
-    y = parseInt(parentNode.id.substr(0,parentNode.id.lastIndexOf(',')));
-    //var vertical = [x-1][y-0]
-    for (var i = 0; i < 1; i++) {
-      for (var j = 0; j <= 1; j++) {
-        console.log(grid[y+j][x-1]);
-      }
-      for (var k = 0; k < 1; k++) {
-        console.log(grid[y-k][x+k]);
+  heuristicDistance:function(grid){
+    var startpoint = grid.startpoint[0];
+    var x = startpoint.xmap;
+    var y = startpoint.ymap;
+  //  var grid = grid.grid;
+
+    for (var i = 0; i < 15; i++) {
+      for (var j = 0; j < 15; j++) {
+        if(grid.walls.indexOf(grid.grid[i][j]) == -1){
+          grid.grid[i][j].heuristic(grid.endpoint);
+          for (var k = 0; k < 1; k++) {
+            for (var l = 1; l > -3; l-=2) {
+              grid.grid[x-l][y-0].calcGScore(10);
+              grid.grid[x-0][y-l].calcGScore(10);
+              grid.grid[x-l][y-1].calcGScore(14);
+              grid.grid[x-l][y+1].calcGScore(14);
+            }
+          }
+        }else{
+          grid.grid[i][j].status = 0;
+        }
       }
     }
+    console.log(grid.grid);
+
   },
 }
